0ad3f76a7bd2d9b1fdc7bbf43a93b795d9422249
EvaCastellano <evacastellano60@gmail.com>
2024-01-18 14:12:18 +0100

    Añado funciones
    funciones de mostrar/esconder la ventana.

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
+
+//  FUNCIONES
+const openModal = function () {  //mostrar ventana = quitar hidden
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+const closeModal = function () {  //esconder ventana = añadir hidden
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
