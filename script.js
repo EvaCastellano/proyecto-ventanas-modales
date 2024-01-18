c988fb9dc25b70db0b898748f2c379917c09cdb2
EvaCastellano <evacastellano60@gmail.com>
2024-01-18 14:13:22 +0100

    Listeners
    asocio un listener a cada botón para abrir y cerrar el modal

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ const closeModal = function () {  //esconder ventana = añadir hidden
   modal.classList.add('hidden')
   overlay.classList.add('hidden')
 }
+
+// asociar un listener a cada botón para abrir y cerrar el modal
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal) //abrir
+
+btnCloseModal.addEventListener('click', closeModal) //cerrar
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function (e) {  //cerrar con la tecla ESC
+  // console.log(e, e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal()
+  }
+})
